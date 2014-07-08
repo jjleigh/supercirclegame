8359544dbcb0936feb7e3d111aa01ce023bf8568
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-08 11:28:28 -0400

    making the circles move

--- game.js
+++ game.js
@@ -1,23 +1,44 @@
-var circles = [];
 
-for(var i=0; i < 10; i++) {
-// create circle and then add to array
-circles.push(new Circle());
-}
 function Circle() {
 	this.x = Math.random() * 450;
 	this.y = Math.random() * 450;
 	this.diameter = 30 + Math.random() * 50;
 	this.speed = 500 + Math.random() * 1500;
-}
-$(document).ready(function() {
-	$('.circle').animate({
-		top: "+=300",
-		left: "+=300"
-		
-	}, {
+
+	this.render = function() {
+		this.$me = $('<div class="circle"></div>')
+			.css('left', this.x)
+			.css('top', this.y)
+			.css('height', this.diameter)
+			.css('width', this.diameter);
+		$('#game').append(this.$me);
+
+	};
+
+	this.move = function() {
+		this.$me.animate ({
+			top: "+=300",
+			left: "+=300"
+		}, {
 		complete: function() {
-			alert("I moved!");
-		}
-	});	
+			this.move();
+		})
+	}
+}
+
+;
+$(document).ready(function() {
+	var circles = [];
+
+	for(var i=0; i < 10; i++) {
+	// create circle and then add to array
+	circles.push(new Circle());
+	circles[i].render
+	circles[i].move
+	}
+	// $('.circle').animate({
+		
+		
+	// },
+	// });	
 });
