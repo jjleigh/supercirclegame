f4c59ec1766e1442a8048a0c1467d13a4c98ceaf
justine jones <justinejones@justines-MacBook-Air.local>
2014-07-08 15:04:39 -0400

    cleaned up the code, added comments, closed all brakets

--- game.js
+++ game.js
@@ -1,35 +1,16 @@
-function Game(circleCount, duration) {
-	this.score =0;
-	this.circles = [];
-	this.duration = (duration || 10) * 1000;
-	this.circleCount = circleCount || 10;
+$(document).ready(function() {
+	window.game = new Game(10, 10);
+	window.game.start();
 
-	this.start = function() {
-	for(var i=0; i < this.circleCount; i++) {
-	// create circle and then add to array
-	circles.push(new Circle());
-	circles[i].render();
-	circles[i].move();
-	}
-
-	$('#score').text(this.score);
-	setTimeout(this.stop, this.duration);
-}
-
-	this.stop = function() {
-		alert('Game over!');
-
-		for (var i = 0; i < this.circleCount; i++) {
-			game.circles[i].$me.remove();
-		}
-	}
-
+});
+// this function sets the size of the circle
 function Circle() {
 	this.x = Math.random() * 450;
 	this.y = Math.random() * 450;
 	this.diameter = 30 + Math.random() * 50;
 	this.speed = 500 + Math.random() * 1500;
 
+//  this function 
 	this.render = function() {
 		var _this = this;
 
@@ -44,47 +25,76 @@ function Circle() {
 
 		$('#game').append(this.$me);	
 	};
+}; 
 
-	this.move = function() {
-		var _this = this;
-		this.$me.animate ({
-			top: Math.random() * 450,
-			left: Math.random() * 450
-		}, {
+// this function moves the bubles in the game 
+this.move = function() {
+	var _this = this;
+	this.$me.animate ({
+		top: Math.random() * 450,
+		left: Math.random() * 450
+	}, {
 		duration: this.speed,
 		complete: function() {
 			_this.move();
-		})
+		}
+	});
+};
+
+// this function kills the bubble once they have been clicked
+this.kill = function() {
+	this.$me.css('background-color', 'red')
+		.effect({
+			effect: 'explode',
+			duration: 100,
+			complete: function() {
+				$(this).remove();
+				$('#score').text(window.game.score += 100);
+			},
+			queue: false
+		});
+};
+//  this deal with creating a new circle
+Circle.init = function() {
+	var circle = new Circle();
+	circle.render();
+	circle.move();
+	return circle;
+};
+
+// this fucntion deals with starting and stopping the game
+function Game(circleCount, duration) {
+this.score =0;
+this.circles = [];
+this.duration = duration * 1000;
+this.circleCount = circleCount;
+
+// this funciton starts the game
+	this.start = function() {
+		for(var i=0; i < this.circleCount; i++) {
+
+			circles.push(new Circle.init());
+			circles[i].render();
+			circles[i].move();
+		}
+
+		$('#score').text(this.score);
+		setTimeout(this.stop, this.duration);
 	};
+//  this function stops the game once its over
+	this.stop = function() {
+		alert('Game over!');
 
-	this.kill = function() {
-		this.$me.css('background-color', 'red')
-			.effect({
-				effect: 'explode',
-				duration: 100,
-				complete: function() {
-					$(this).remove();
-					$('#score').text(window.game.score += 100);
-				},
-				queue: false
-			});
+		for (var i = 0; i < this.circleCount; i++) {
+			game.circles[i].$me.remove();
+		}
 	};
-}
+};
 
+	// var circles = [];
+	// for(var i=0; i < 10; i++) {
 
-;
-$(document).ready(function() {
-	var circles = [];
-
-	for(var i=0; i < 10; i++) {
-	// create circle and then add to array
-	circles.push(new Circle());
-	circles[i].render
-	circles[i].move
-	}
-	// $('.circle').animate({
-		
-		
-	// },
-	// });	
-});
+	// circles.push(new Circle());
+	// circles[i].render
+	// circles[i].move
+	// }
